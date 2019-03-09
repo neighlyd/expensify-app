1b74f422ac43bba6c76e32d4f88e7afcaa29e431
Dustin Neighly <neighlyd@gmail.com>
2019-03-08 16:50:20 -0800

    Added numeral module to format money

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import moment from 'moment';
+import numeral from 'numeral';
 import { Link } from 'react-router-dom';
 
 
 const ExpenseItem = ({ dispatch, id, description, amount, createdAt }) => (
     <div>
-        <Link to={`/edit/${id}`}><h3>Description: { description}</h3></Link>
-        <p>Amount: ${ amount / 100}</p>
-        <p>Created At: { moment(createdAt).format('MMM Do, YYYY') }</p>
-        
+        <Link to={`/edit/${id}`}>
+            <h3>{description}</h3>
+        </Link>
+        <p>
+            { numeral(amount / 100).format('$0,0.00') }
+             - 
+            { moment(createdAt).format('MMM Do, YYYY') }
+        </p>        
         <hr/>
     </div>
 );
