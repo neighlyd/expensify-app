245d62c59f6646f1d8fece05618c50049c84dd51
Dustin Neighly <neighlyd@gmail.com>
2019-04-09 16:44:32 -0700

    Secured private and public routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,10 +4,10 @@ import createHistory from 'history/createBrowserHistory'
 import AddExpensePage from '../components/AddExpense'
 import EditExpensePage from '../components/EditExpense'
 import ExpenseDashboardPage from '../components/ExpenseDashboard'
-import HelpPage from '../components/Help'
 import LoginPage from '../components/LoginPage'
 import NotFoundPage from '../components/NotFound'
 import PrivateRoute from './PrivateRoute'
+import PublicRoute from './PublicRoute'
 
 export const history = createHistory()
 
@@ -15,11 +15,10 @@ const AppRouter = () => (
     <Router history={ history }>
         <div>
             <Switch>
-                <Route exact path="/" component={LoginPage}/>
+                <PublicRoute exact path="/" component={LoginPage}/>
                 <PrivateRoute path="/dashboard" component={ExpenseDashboardPage}/>
                 <PrivateRoute path="/create" component={AddExpensePage}/>
                 <PrivateRoute path="/edit/:id" component={EditExpensePage}/>
-                <Route path="/help" component={HelpPage}/>
                 <Route component={NotFoundPage}/>
             </Switch>
         </div>
