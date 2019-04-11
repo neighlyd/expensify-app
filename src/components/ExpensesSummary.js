c46158de1085ea438ad74f815b57b50224b8e874
Dustin Neighly <neighlyd@gmail.com>
2019-04-11 15:39:00 -0700

    Hidden filters feature

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -6,14 +6,16 @@ import selectExpenses from '../selectors/expenses'
 import selectExpensesTotal from '../selectors/expenses-total'
 
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
-    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses'
+export const ExpensesSummary = ({ hiddenExpenses, expenseCount, expensesTotal }) => {
+    const filteredExpenseWord = expenseCount === 1 ? 'expense' : 'expenses'
+    const hiddenExpenseWord = hiddenExpenses === 1 ? 'expense' : 'expenses'
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00')
     
     return (
         <div className="page-header">
             <div className="container">
-                <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+                <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {filteredExpenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+                <p className="page-header__sub-title">(Hiding {hiddenExpenses} {hiddenExpenseWord})</p>
                 <div className="page-header__actions">
                     <Link to="/create" className="button">Create Expense
                     </Link>
@@ -25,8 +27,10 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
 
 const mapStateToProps = (state) => {
     const visibleExpenses = selectExpenses(state.expenses, state.filters)
+    const hiddenExpenses = state.expenses.length - visibleExpenses.length
     
     return {
+        hiddenExpenses,
         expenseCount: visibleExpenses.length,
         expensesTotal: selectExpensesTotal(visibleExpenses)
     }
